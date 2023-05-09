5645c9a0becad68fa5ee11880c4addb42ab6ed86
jackbill749 <jackbill749@gmail.com>
2023-05-09 23:45:50 +0800

    从本地提交完整工程

---
+++ src/main/java/org/ghost/Main.java
@@ -0,0 +1,13 @@
+package org.ghost;
+
+/**
+ * @author ZhaoJinTong
+ * @version V2.0
+ * @Description:
+ * @date 2022/6/13 23:20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
